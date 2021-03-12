181c27d986b11194fe1ddf95e0f769b439baa2d3
kdraa12 <you@example.com>
2021-03-12 19:55:18 +0000

    deposit age

--- custumers/views.py
+++ custumers/views.py
@@ -6,6 +6,7 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
+from alimentation.models import TopUp
 from custumers.models import Manager, Customer
 from custumers.models_serializers import ManagerSerializer, CustomerSerializer, UserSerializer
 
@@ -24,6 +25,25 @@ class ManagerViewSet(viewsets.ModelViewSet):
         m = Manager.objects.filter(user=request.user.id).first()
         mngr = ManagerSerializer(m, context={'request': request})
         return Response(mngr.data)
+
+    @action(detail=True, methods=['post'])
+    def balance(self, request, pk=None):
+        m = Manager.objects.filter(user=request.user.id).first()
+        topups=TopUp.objects.filter(maker=m,withdrawed=False).all()
+
+        total = 0
+        for topup in topups:
+            total+=TopUp.amount
+
+        return Response({'total': total})
+
+
+
+
+        mngr = ManagerSerializer(m, context={'request': request})
+        return Response(mngr.data)
+
+
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
