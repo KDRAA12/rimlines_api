bc8124f2ee481c3d95cd62f718a3b5b025703ee2
kdraa12 <you@example.com>
2021-05-03 00:09:41 +0000

    order good

--- custumers/admin.py
+++ custumers/admin.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from orders.models import Order, Payment, Report, Product, LineItem
+from custumers.models import Customer, Manager
 
-admin.site.register(Order)
-admin.site.register(Payment)
-admin.site.register(Report)
-admin.site.register(Product)
-admin.site.register(LineItem)
-
+admin.site.register(Manager)
+admin.site.register(Customer)
 
 
 
--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from orders.models import Order, Payment, Report, Product, LineItem
+
+admin.site.register(Order)
+admin.site.register(Payment)
+admin.site.register(Report)
+admin.site.register(Product)
+admin.site.register(LineItem)
+
+
+
+
+
